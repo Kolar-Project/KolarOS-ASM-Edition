4a8a91c56c6ec86b37f9fa7eebd7e8168ad26203
GreenSoupDev <109924369+GreenSoupDeveloper@users.noreply.github.com>
2023-05-21 11:30:38 -0400

    Delete compile.bat

--- compile.bat
+++
@@ -1,6 +0,0 @@
-nasm -f bin -o C:\asmkolar\kolar.bin C:\asmkolar\boot.asm
-
-copy /b C:\asmkolar\kolar.bin c:\asmkolar\cdiso\kolar.flp
-
-
-C:\asmkolar\mkisofs.exe -no-emul-boot -boot-load-size 4 -o C:\asmkolar\kolar.iso -b kolar.flp C:/asmkolar/cdiso/
