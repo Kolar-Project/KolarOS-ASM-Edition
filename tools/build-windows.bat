d3c2b3dde8fc95acdc03a5a4c570171ff9d646e0
GreenSoupDev <109924369+GreenSoupDeveloper@users.noreply.github.com>
2023-06-03 19:37:12 -0400

    Delete tools directory

--- tools/build-windows.bat
+++
@@ -1,7 +0,0 @@
-nasm -f bin -o C:/asmkolar/bin/kolar.bin C:/asmkolar/src/boot.asm
-
-copy /b C:\asmkolar\bin\kolar.bin c:\asmkolar\bin\kolar.flp
-
-C:\asmkolar\tools\mkisofs.exe -no-emul-boot -boot-load-size 4 -o C:\asmkolar\bin\kolar.iso -b kolar.flp C:/asmkolar/bin/
-
-echo Done!
